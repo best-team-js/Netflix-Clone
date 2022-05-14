bc7fdc0498af0223a03146bdfa7aba4e3a8a3e03
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-05-14 09:52:20 +0300

    done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,28 +5,28 @@ import MovieList from '../MovieList/MovieList';
 
 
 export default function Home() {
-    const [recipes, setRecipes] = useState([]);
-    async function getRecipes() {
+    const [movies, setMovies] = useState([]);
+    async function getMovies() {
         //let serverUrl = process.env.REACT_KEY_SERVER;
         // let response = await fetch(serverUrl);
 
         let url = 'https://movies-recipe.herokuapp.com';
         let response = await fetch(`${url}/trending`);
         console.log("response", response);
-        let recipesData = await response.json();
-        setRecipes(recipesData);
-        console.log("1, After updating", recipes);
+        let moviesData = await response.json();
+        setMovies(moviesData);
+        console.log("1, After updating", movies);
     }
 
     useEffect(() => {
-        getRecipes();
+        getMovies();
     }, [])
 
     return (
         <>
-            <Button variant="info">Get Recipes</Button>
+             <Button variant="danger">Get Movies</Button>
             {
-                (recipes.length > 0) && <MovieList recipes={recipes} />
+                (movies.length > 0) && <MovieList movies={movies} />
             }
 
 

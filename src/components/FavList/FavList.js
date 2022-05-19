cc44d7c3050bb416d1c88fcfe88d2abb1ca21a8b
HebaAlhamaydh <Eng_heba_hamaydh@hotmail.com>
2022-05-19 08:06:04 +0300

    lastchanges

--- src/components/FavList/FavList.js
+++ src/components/FavList/FavList.js
@@ -1,68 +1,71 @@
-    import { useState, useEffect } from 'react';
-    import Card from 'react-bootstrap/Card';
-    import Button from 'react-bootstrap/Button';
-    
-    export default function FavList() {
-        const [favMovies, setFavMovies] = useState();
-    
-        async function getFavMovies() {
- // let url = 'https://movies-recipe.herokuapp.com';
-           
-            // `${url}/getMovies`
-        let url = `${process.env.REACT_APP_SERVER}/getMovies`
-            let response = await fetch(url, {
-                method: 'GET'
-            });
-    
-            let recivedData = await response.json();
-            setFavMovies(recivedData);
-        }
-    
-        async function handleDelete(id) {
-            let url = 'https://movies-recipe.herokuapp.com';
-            
-            
-            let response = await fetch(`${url}/delete/${id}`, {
-                method: 'DELETE',
-            })
+import { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
-    
-            if (response.status == 204) {
-                getFavMovies();
-                alert("movies deleted successfully");
-            }
-    
-        }
-    
-    
-        useEffect(() => {
+export default function FavList() {
+    const [favMovies, setFavMovies] = useState();
+
+    async function getFavMovies() {
+ let url = 'https://movies-recipe.herokuapp.com/getMovies';
+       
+        // `${url}/getMovies`
+    // let url = `${process.env.REACT_APP_SERVER}/getMovies`
+        let response = await fetch(url, {
+            method: 'GET'
+        });
+
+        let recivedData = await response.json();
+        setFavMovies(recivedData);
+    }
+
+    async function handleDelete(id) {
+        let url = `https://movies-recipe.herokuapp.com/delete?id=${id}`;
+        let response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+          
+            });
+        
+        if (response.status == 204) {
             getFavMovies();
-        }, []);
-        return (
-            <>
-                <h1 align="center">Favourite Movies </h1>
-                {
-                    favMovies && favMovies.map((favMovie) => {
-                        return (
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${favMovie.poster_path}`}   />
-                               
-                                <Card.Body>
-                                    <Card.Title>{favMovie.title}</Card.Title>
-                                    <Card.Text>
-                                        {favMovie.overview}
-                                    </Card.Text>
-                                    <Button variant="danger" onClick={()=>handleDelete(favMovie.id)}>Delete</Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    }
+            alert("movies deleted successfully");
+        }
+
+    }
+    useEffect(() => {
+        getFavMovies();
+    }, []);
+    return (
+        <>
+            <h1 align="center">Favourite Movies </h1>
+            {
+                favMovies && favMovies.map((favMovie) => {
+                    return (
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${favMovie.poster_path}`}   />
+                           
+                            <Card.Body>
+                                <Card.Title>{favMovie.title}</Card.Title>
+                                <Card.Text>
+                                    {favMovie.overview}
+                                </Card.Text>
+                                <Card.Text>
+                             {favMovie.comment}
+                               </Card.Text>
+                                <Button variant="danger" onClick={()=>handleDelete(favMovie.id)}>Delete</Button>
+                            </Card.Body>
+                        </Card>
                     )
                 }
-    
-            </>
-        )
-    }
-    
-    
+                )
+            }
+
+        </>
+    )
+}
+
 
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -5,8 +5,6 @@ import ModalMovie from "../ModalMovie/ModalMovie"
 
 // import {Card , Button} from 'react-bootstrap'
 
-
-
 export default function Movie(props) {
     const [show, setShow] = useState(false);
     const [chosenMovie, setChosenMovies] = useState();
@@ -20,7 +18,7 @@ export default function Movie(props) {
     return (
         <>
            <Card style={{ width: '18rem', textAlign: "center", marginTop: "3rem", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }}>
-                <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`} />
+                <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} />
                 <Card.Body className='cardBody'>
                     <Card.Title>{props.movie.title}</Card.Title>
                     <Card.Text className="scrollBar">
